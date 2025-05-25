381dff5c6256d236e9af3b941584a6be7bd43646
emillllll64 <guilhermefernandessousa46@gmail.com>
2025-05-25 18:12:00 +0000

    initial commit

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun May 25 17:20:55 UTC 2025"
+#define LINUX_COMPILE_BY "8letras"
+#define LINUX_COMPILE_HOST ""
+#define LINUX_COMPILER "Android (12833971, +pgo, +bolt, +lto, +mlgo, based on r536225) clang version 19.0.1 (https://android.googlesource.com/toolchain/llvm-project b3a530ec6537146650e42be89f1089e9a3588460), LLD 19.0.1"
